433934637335f7c5b5bce83c065193f0ba3fec8f
Abishek V Ashok <abhi2424shekvashok@gmail.com>
2017-01-11 07:14:48 +0530

    Separating class.

--- lib/img_checker.rb
+++ lib/img_checker.rb
@@ -1,3 +1,4 @@
+
 # img_checker ruby gem
 # This gem checks for images which exceept the specified limit in the
 # img_config.yml file.
@@ -16,60 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-class ImgChecker
-  def initialize(config_file)
-    print 'Starting Image Checker,'
-    print ' authored by Abishek V Ashok "for" FOSSASIA with love'
-    puts "Improvements by Robby O'Connor during GCI 2016"
-    # Initialize the config file
-    require 'yaml'
-    @config = YAML.load(File.open(config_file))
-    # This int is to count the number of big images that exceed the specified
-    # widthor height
-    @big_images = 0
-  end
-
-  # Check the image dimensions
-  def check_dimensions?(image, width, height)
-    require 'fastimage'
-    image_size = FastImage.size(image)
-    if image_size[0] > width || image_size[1] > height
-      puts "The image #{image} is larger than #{width}px x #{height}px [w x h]"
-      @big_images += 1
-      return false
-    end
-    true
-  end
-
-  def check_images?
-    valid_sizes = true
-    # For each direcotry hash do
-    @config.each do |dir_config|
-      # Get the directory pathonly from the array
-      directory = Dir[dir_config['directory']]
-      height = dir_config['height']
-      width = dir_config['width']
-      directory.each do |image|
-        # Now we will use the other data available in the dir_config array
-        valid_sizes = false unless check_dimensions?(image, width, height)
-      end
-    end
-    valid_sizes
-  end
-
-  def ok?
-    if check_images?
-      puts 'All images are ok... Hurray!'
-    else
-      big_images = @big_images
-      puts 'Image Checker: These images exceeds the specified dimensions'
-      abort "#{big_images} image(s) have greater dimensions then specified."
-      return false
-    end
-    true
-  end
-end
-
+require 'img_checker/img_checker'
 config_file = './img_config.yml'
 
 # Check images only if the config file exists or exit throwing out an error
