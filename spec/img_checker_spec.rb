dd76960dbc1545bf3a1762cfbb1c15dfd22b3053
Abishek V Ashok <abhi2424shekvashok@gmail.com>
2017-01-12 07:16:49 +0530

    Updated the rake tests

--- spec/img_checker_spec.rb
+++ spec/img_checker_spec.rb
@@ -1,2 +1,17 @@
 # Rsepc tests for img_checker ruby gem
-require_relative '../lib/img_checker.rb'
+require 'img_checker/iimg_checker.rb'
+
+Rspec.describe ImgChecker, "This should check for images when the config file is parsed"
+  context "and checks the image dimensions"
+    it "and handles errors as well"
+      CONFIG_FILE = './img_config.yml'.freeze
+      if File.exist?(CONFIG_FILE)
+        require_relative '../lib/img_checker/img_checker.rb'
+        checker = ImgChecker.new CONFIG_FILE
+        checker.ok?
+      else
+        abort 'Critical Error: img_config.yml not found. Please add to the current directory.' # rubocop: disable Metrics/LineLength
+      end
+    end
+  end
+end
